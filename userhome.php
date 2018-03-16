87af8ad278a6667dc7652c19cc8259303f820711
sriram <sriramprasadtalasila@gmail.com>
2018-03-16 22:28:26 +0530

    Update userhome.php

--- userhome.php
+++ userhome.php
@@ -1,12 +1,14 @@
 <?php
 	require 'scripts/connect.php';
 	session_start();
+	$usr = $_SESSION['username'];
 	$sql = "SELECT * FROM `products`";
 	$res = mysqli_query($conn,$sql);
 	if(isset($_SESSION['username'])){
 		if($_SESSION['role'] !== 'U')
 			header('location : userlogin.php');
 	}
+	
 	
 ?>
 <html>
@@ -26,13 +28,43 @@
 		}
 		
 	</style>
-	<script>
-		$(document).ready(function(){
-			    $.fn.MessageBox = function () {
-			    var Qkey = $(this).data("sample-id");
-			    console.log('QKey';)
-	}
-		})
+	<script type="text/javascript">
+		
+		function displayC(pid){
+			$("#addc").empty();
+			$.getJSON("sendc.php",{id:pid}).done(function(res){
+				$.each(res.result, function(i, result) {
+					console.log(result.pid);
+					content="<p><strong>"+result.uname+"</strong></p>";
+					content+="<p>"+result.feedback+"</p><hr>";
+					$(content).appendTo("#addc");
+				})
+			});
+		}
+		
+
+		function myFunction(pid) {
+			displayC(pid);
+			$("#cbtn").click(function(){
+				var cmt = $("#comnt").val();
+				dta={
+					id:pid,
+					feedback:cmt
+				}
+				console.log(dta);
+				if(pid!=0||pid!=null){
+					$.post("uploadC.php", dta).done(function(data){
+						displayC(pid);
+						pid=null;
+					})
+				}
+					
+			})
+		}
+				
+	
+
+		
 	</script>
 	<body>
 		<div class="navbar">
@@ -47,7 +79,7 @@
 			<?php if($res){
 				while($row= mysqli_fetch_array($res)){
 					echo "<div class='styl'><img width='200px' height='auto' src='images/".$row[3]."'>";
-					echo "<p><strong>Name:</strong>".$row[1]."</br><strong>Description:</strong>".$row[2]."</p></div><button data-toggle='modal' onclick= 'chm(".$row[0].",'".$_SESSION['username']."')' data-target='#mymodal'>Comment</button><hr>";
+					echo "<p><strong>Name:</strong>".$row[1]."</br><strong>Description:</strong>".$row[2]."</p></div><button data-toggle='modal' onclick='myFunction(".$row[0].");' data-target='#mymodal'>Comment</button><hr>";
 		}
 	}?>	
 		</div>
@@ -55,11 +87,15 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
+
 				<h4>Comments</h4>
 					<button data-dismiss="modal" class="close" data-target="#mymodal">&times;</button>	
-					<div id="addc" class="modal-body">
-					
 					</div>
+					<div  class="modal-body">
+						<div id="addc"></div>
+						<textarea row="3" col="4" id ="comnt" ></textarea>
+						<button style="float:right"id="cbtn">Comment</button>
+					
 				</div>
 			</div>
 		</div>
